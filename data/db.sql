964627d0f1b7a28f3611f8e1adfa0d93a4e65c77
Noël <noel.rodriguez@hotmail.fr>
2019-04-30 21:29:37 +0200

    Mise a jour db

--- data/db.sql
+++ data/db.sql
@@ -1,6 +1,7 @@
 CREATE TABLE "user" (
 	id SERIAL PRIMARY KEY,
-	pseudo VARCHAR NOT NULL,
+	pseudo VARCHAR UNIQUE,
+	password VARCHAR NOT NULL,
 	address VARCHAR NOT NULL,
 	city_id INTEGER NOT NULL,
 	description TEXT
@@ -48,7 +49,7 @@ CREATE TABLE "delivery_system" (
 	id SERIAL PRIMARY KEY,
 	description TEXT,
 	price FLOAT NOT NULL
-);
+)	
 
 /* Jointures */
 CREATE TABLE "product_category" (
@@ -70,3 +71,34 @@ CREATE TABLE "purchase" (
 	delivery_system_id INTEGER,
 	PRIMARY KEY(product_id, buyer_id, pdate);
 );
+
+
+
+INSERT INTO "user" VALUES
+(1, "Toto", "Toto", "30 Ruelle du Pingouin", "1", "First"),
+(2, "Jean-Eude", "Argent", "5 Faubourg de la Cathédrale", "2", "Je suis riche");
+
+INSERT INTO "city" VALUES
+(1, "Evry", 1),
+(2, "Paris", 1),
+(3, "Province", 1);
+
+INSERT INTO "planet" VALUES
+(1, "Terre"),
+(2, "Mars"),
+(3 "KinderBueno");
+
+INSERT INTO "product" VALUES
+(1, 2, "Tarte", "Le sucre c'est bon", 3.14, 10),
+(2, 1, "Pelle", "Ca creuse bien!", 10, 1);
+
+INSERT INTO "category" VALUES
+("Nourriture", "Tout ce qui se mange", NULL),
+("Outils", "Ici on bricole", NULL);
+
+INSERT INTO "delivery_system" VALUES
+(1, "Transport par FaidecsShip", 10);
+
+INSERT INTO "product_category" VALUES
+(1, "Nourriture"),
+(2, "Outils");
