c99d6a8d3ced9bad94660db856cf253b03265da7
ggabimoran <gabrielmoran7@live.fr>
2019-05-01 20:42:28 +0200

    working on authentification

---
+++ ensiie-project-master/public/VerifMDP.php
@@ -0,0 +1,31 @@
+<?php
+
+include("Vue.php");
+
+$psd = $_POST['psd']; //pseudo 
+$mdp = $_POST['mdp']; //mot de passe
+
+enTete("Vérification du mot de passe");
+
+/*
+  Si le mot de passe entré est vide ou faux (cf. fonction verif_mdp), afficher une erreur et un lien vers tpConnexion.php
+ 
+  S'il est bon, rediriger vers la page index.php
+*/
+
+function verif_mdp($psd,$mdp) {
+
+	if ($db = db_connect()) {
+		$_SESSION['mdp']=$mdp;
+		db_close($db);
+		<a href="Connexion.php">Page d'authentificatoin</a>
+		return true;
+	} else
+		return false;
+
+}
+
+verif_mdp($psd,$mdp);
+pied();
+
+?>
---
+++ ensiie-project-master/src/User/User.php
@@ -0,0 +1,135 @@
+<?php
+namespace User;
+
+class User
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $pseudo;
+
+    /**
+     * @var string
+     */
+    private $address;
+
+    /**
+     * @var int
+     */
+    private $city_id;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    private $passeword;
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return User
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+
+    /**
+     * @param string $pseudo
+     * @return User
+     */
+    public function setPseudo($pseudo)
+    {
+        $this->pseudo = $pseudo;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param string $address
+     * @return User
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCityId()
+    {
+        return $this->city_id;
+    }
+
+    /**
+     * @param int $city_id
+     * @return User
+     */
+    public function setCityId($city_id)
+    {
+        $this->city_id = $city_id;
+        return $this;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }  
+
+    /**
+     * @param string $description
+     * @return User
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+
+    } 
+
+    public function getPasseword()
+    {
+        return $this->passeword;
+    }
+
+    public function setPasseword($passeword)
+    {
+        $this->passeword = $passeword;
+        return this;
+    }
+}
+
