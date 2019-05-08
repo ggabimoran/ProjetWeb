f7eb24877d8e6aae354686586d77b6e45b524d8a
尤子轩 <YZX@youzixuandeMacBook-Pro.local>
2019-05-08 17:26:01 +0200

    a

--- public/VerifMDP.php
+++ public/VerifMDP.php
@@ -2,6 +2,7 @@
 
 include("Vue.php");
 include("../src/User/User.php");
+include("../src/User/UserRepository.php");
 
 $psd = $_POST['psd']; //pseudo 
 $mdp = $_POST['mdp']; //mot de passe
@@ -13,17 +14,27 @@ enTete("Vérification du mot de passe");
  
   S'il est bon, rediriger vers la page index.php
 */
-if(checkPassword($psd, $mdp)) {
+  //$user = new \User\User($psd);
+
+  $dbName = getenv('DB_NAME');
+  $dbUser = getenv('DB_USER');
+  $dbPassword = getenv('DB_PASSWORD');
+  $connection = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
+  $userRepository = new \User\UserRepository($connection);
+
+  $password = $userRepository->getPassword($psd);  
+  if($password == $mdp) {
 	//config();
-	header('Location: index.php');
-}
-else {
-	affiche_erreur("Le mot de passe entré est eronné.");
-    affiche_info('Veuillez-essayer de nouveau <a href="tpConnexion.php">ici</a>.');
-}
+  	echo'<meta http-equiv="Location" content="Connexion.php">';
+  	exit();
+  }
+  else {
+  	affiche_erreur("Le mot de passe entré est eronné.");
+  	affiche_info('Veuillez-essayer de nouveau <a href="Connexion.php">ici</a>.');
+  }
 
 
 
-pied();
+  pied();
 
-?>
+  ?>
