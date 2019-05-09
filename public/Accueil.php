7dda4e8aded17da7e23980cf5a0d110831d19728
ggabimoran <gabrielmoran7@live.fr>
2019-05-09 13:32:47 +0200

    modification

--- public/Accueil.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'Vue.php';
-enTete("Selectionnez votre lieu.");
-vue_lieu();
-retour_menu();
-pied();
-?>
--- public/index.php
+++ public/index.php
@@ -8,38 +8,9 @@ $dbUser = getenv('DB_USER');
 $dbPassword = getenv('DB_PASSWORD');
 $connection = new PDO("pgsql:host=postgres user=$dbUser dbname=$dbName password=$dbPassword");
 
-$userRepository = new \User\UserRepository($connection);
-$users = $userRepository->fetchAll();
-enTete('Bienvenue sur le site intergalactique de gré à gré!');
-?>
 
-<div class="container">
-    <table class="table table-bordered table-hover table-striped">
-        <thead style="font-weight: bold">
-            <td>CityId</td>
-            <td>Pseudo</td>
-            <td>Adress</td>
-        </thead>
-        <?php /** @var \User\User $user */
-        foreach ($users as $user) : ?>
-        <tr>
-            <td><?php echo $user->getCityId() ?></td>
-            <td><?php echo $user->getPseudo() ?></td>
-            <td><?php echo $user->getAddress() ?></td>
-        </tr>
-        <?php endforeach; ?>
-    </table>
-</div>
-
-<nav>
-    <a href="Connexion.php">Connexion</a>
-    <a href="Accueil.php">Accueil</a>
-</nav>
-
-<footer>
-	<p>Informations complémentaires :</p>
-</footer>
-<?php
+enTete("Selectionnez votre lieu.");
+vue_lieu();
 retour_menu();
 pied();
 ?>
